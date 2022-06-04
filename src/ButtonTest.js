81fe8be184f1ab7b855ac9f666cf806860200fec
Jeonjunhowi <jjh63360@kau.kr>
2022-06-04 17:04:41 +0900

    add: hitSlop && Retention Offset Options

--- src/ButtonTest.js
+++ src/ButtonTest.js
@@ -3,7 +3,10 @@ import {TouchableOpacity, View, Text} from 'react-native';
 
 const MyButton = () => {
     return (
-        <TouchableOpacity onPress={() => alert('MyButton Clicked!')}>
+        <TouchableOpacity 
+            onPress={() => alert('MyButton Clicked!')}
+            hitSlop={{bottom:10, top:10, left:10, right:10}}
+            pressRetentionOffset={{bottom:30, top:30, left:80, righ:80}}>
             <View style={{backgroundColor: 'yellow', padding: 10}}>
                 <Text style={{fontSize: 20, color: 'blue'}}>MyButton</Text>
             </View>
